e3352fdc0f728dab55b4157c16af1d44fc7ac925
moukhtar-b <gbenhatchi@hotmail.fr>
2017-02-10 23:40:52 +0000

    sezar function

--- Crypto-Sezar/main.cpp
+++ Crypto-Sezar/main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int sezar_char(char c , int n){
+    if (c >= 'A'  && c <= 'Z')
+        return  (c % 65) +n + 65;
+    if (c >= 'a'  && c <= 'z')
+        return  (c % 97) +n + 97;
+}
+
+int main(int argc,char *argv[]) {
+
+    printf("%c \n",sezar_char('f',2));
+    printf("%c \n",sezar_char('c',2));
+    printf("%c \n",sezar_char('j',2));
+    printf("%c \n",sezar_char('J',2));
+    printf("%c \n",sezar_char('m',2));
     return 0;
 }
+
